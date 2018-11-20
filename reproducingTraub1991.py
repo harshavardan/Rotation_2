fc6fd1cbf5ab1ffac95c365a8dec7406792bda8b
Harshavardan B N <harshavardanbn@ncbs.res.in>
2018-11-20 18:17:11 +0530

    Na and K_DR added

--- reproducingTraub1991.py
+++ reproducingTraub1991.py
@@ -1,52 +1,32 @@
-#import moose
-#import pylab
-#import rdesigneur as rd
-#moose.reinit()
-#rdes = rd.rdesigneur(
-    ## cellProto syntax: ['ballAndStick', 'name', somaDia, somaLength, dendDia, dendLength, numDendSegments ]
-    ## The numerical arguments are all optional
-    #cellProto = [['ballAndStick', 'soma', 8.46e-6, 125e-6, 2.89e-6, 120e-6, 8]],
-    #chanProto = [
-        #['make_Na()', 'Na'],
-        #['make_K_DR()', 'K_DR']
-        #],
-    #chanDistrib = [
-        #['Na', 'soma', 'Gbar', '1200' ],
-        #['K_DR', 'soma', 'Gbar', '360' ],
-        #['Na', 'dend#', 'Gbar', '400' ],
-        #['K_DR', 'dend#', 'Gbar', '120' ]
-        #],
-    #stimList = [['soma', '1', '.', 'inject', '(t>0.01 && t<0.06) * 1e-9' ]],
-    #plotList = [['soma', '1', '.', 'Vm', 'Membrane potential']],
-    ##moogList = [['#', '1', '.', 'Vm', 'Vm (mV)']]
-#)
-#rdes.buildModel()
-#soma = moose.element( '/model/elec/soma' )
-#moose.start(0.07)
-#rdes.display()
-##rdes.displayMoogli( 0.0005, 0.06, 0.0 )
-
+#Reproducing Traub 1991. I wrote an swc file with an apical dendrite and a basal dendrite with dimentions from the paper. The area of the cyclidrical soma from the paper was used to find the radius of a sphere with the same area. This area was used in the swc file. So far, only Na and K_DR channels have been added.
 import moose
 import pylab
 import rdesigneur as rd
 rdes = rd.rdesigneur(
+    cellProto = [
+        ['./cells/traub1991.swc','elec'],
+        #['ballAndStick','soma', 8.46e-6, 125e-6, 5.78e-6, 120e-6, 10]#[ballAndStick,name, somaDia=10e-6, somaLen=10e-6, dendDia=4e-6, dendLen=200e-6, numDendSeg=1] 
+        ],
     chanProto = [
-        ['make_Na()','Na'],
-        ['make_K_DR()','K_DR'],
-        #['./chans/kdr.xml'],
-        #['make_HH_Na()', 'HH_Na'], 
-        #['make_HH_K()', 'HH_K']
+        ['make_Na()', 'Na'],
+        ['make_K_DR()', 'K_DR'],
+        ['make_Ca()', 'Ca'],
         ],
     chanDistrib = [
         ['Na', 'soma', 'Gbar', '30' ],
-        ['K_DR', 'soma', 'Gbar', '25' ]
+        ['K_DR', 'soma', 'Gbar', '15' ],
+        ['Na', 'dend#', 'Gbar', '(p<=12) ? 15 : ((p>12 && p<=24) ? 0 : ((p>24 && p<=36) ? 20 : 0))'],
+        ['K_DR', 'dend#', 'Gbar', '(p<=12) ? 15: ((p>12 && p<=24) ? 5 : ((p>24 && p<=36) ? 0 : (p>36 && p<=48) ? 48 : (p>48 && p<=60) ? 20 : 0))'],
+        ['Na', 'apical#', 'Gbar', '(p<=12) ? 15 : ((p>12 && p<=24) ? 0 : (p>24 && p<=36) ? 20 : 0)'],
+        ['K_DR', 'apical#', 'Gbar', '(p<=12)? 5 : (p>12 && p<=24) ? 0 : (p>24 && p<=36) ? 20 : 0)']
         ],
-    stimList = [['soma', '1', '.', 'inject', '(t>0.1 && t<0.2) * 1e-8' ]],
-    plotList = [['soma', '1', '.', 'Vm', 'Membrane potential']]
+    stimList = [['soma', '1', '.', 'inject', '(t>0.1 && t<0.6) * 1e-10' ]],
+    plotList = [['soma', '1', '.', 'Vm', 'Membrane potential']],
+    #moogList = [['#', '1', '.', 'Vm', 'Soma potential']]
 )
 
 rdes.buildModel()
 moose.reinit()
-moose.start( 0.3 )
+moose.start( .7 )
+#rdes.displayMoogli( 0.001, 0.3, rotation = 0.02 )
 rdes.display()
-#plt.savefig( '%s.png' % __file__ )
